6306a1348f1d07ebfb9e2a7e968c7b0ea884b0d9
gon2santos <necchigonzalo@gmail.com>
2023-01-24 18:14:50 -0300

    feat(login): impl login

--- components/login.jsx
+++ components/login.jsx
@@ -1,13 +1,56 @@
 import styles from '../styles/Home.module.css';
 import React from 'react';
 import { useState } from 'react';
-import { useGetProjectsQuery, useGetTasksQueueQuery, useDeleteTasksMutation, useSetCurrProjMutation } from '../redux/apiSlice';
+import { useLoginUserMutation, useCreateUserMutation } from '../redux/apiSlice';
 import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
 
 export default function Login() {
 
+    const router = useRouter()
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [toggleCreate, setToggleCreate] = useState(false);
+    const [login, response_login] = useLoginUserMutation();
+    const [create, response_create] = useCreateUserMutation();
+    const [loginMsg, setLoginMsg] = useState("");
+
+    function HandleLoginUser() {
+        login({ email: userName, pwd: password }).then(() => { }).catch((error) => {
+            console.log("HandleLoginUser error: " + error)
+        })
+    }
+
+
+    function HandleCreateUser() {
+        create({ email: userName, pwd: password }).then(() => { }).catch((error) => {
+            console.log("HandleCreateUser error: " + error)
+        })
+    }
+
+    useEffect(() => {
+        if(response_login?.data?.msg === "LOGIN_SUCCESS"){
+            setLoginMsg("Log in successful");
+            localStorage.setItem("accessToken", response_login?.data?.accessToken);
+            localStorage.setItem("userEmail", userName);
+            router.push('/');
+        }
+        else if(response_login?.data?.msg === "LOGIN_FAIL" || response_login?.data?.msg === "USER_NOT_FOUND"){
+            setLoginMsg("Incorrect Email or Password");
+        }
+        else if(response_create?.data?.msg === "USER_CREATED"){
+            setLoginMsg("Account created, please log in");
+            setToggleCreate(!toggleCreate);
+        }
+      }, [response_login, response_create]);
+
+    /* if(response_login?.data?.msg === "LOGIN_SUCCESS"){
+        setLoginMsg("Log in successful");
+    }
+    else if(response_login?.data?.msg === "LOGIN_FAIL"){
+        setLoginMsg("Incorrect Email or Password");
+    } */
 
     return (
         <div>
@@ -26,7 +69,12 @@ export default function Login() {
             <div className={styles.username}><label htmlFor="userName"><h2>User:</h2></label><input autoFocus className={styles.inputBox} name='userName' value={userName} placeholder='email address' onChange={(e) => setUserName(e.target.value)} /></div>
             <div className={styles.password}><label htmlFor="password"><h2>Password:</h2></label><input type='password' className={styles.inputBox} name='password' value={password} placeholder='*************' onChange={(e) => setPassword(e.target.value)} /></div>
 
-            <button className={styles.loginButton}><h2>Login</h2></button>
+            <button className={styles.loginButton} onClick={() => toggleCreate ? HandleCreateUser() : HandleLoginUser()}>{!toggleCreate ? <h2>Login</h2> : <h2>Create Account</h2>}</button>
+
+            {<h3 style={{color:'rgb(185, 187, 59)'}}>{loginMsg}</h3>}
+
+            <div className={styles.createAccountButton}>{!toggleCreate ? <div onClick={() => setToggleCreate(!toggleCreate)}>Not registered? Create an account</div> : <div onClick={() => setToggleCreate(!toggleCreate)}>Already have an account? Login</div>}</div>
+            
         </div>
     )
 }
